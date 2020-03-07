b6afc07754c431863a6cd5affbe489b83c42379b
samikshamodi <55681233+samikshamodi@users.noreply.github.com>
2020-03-07 21:03:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,19 @@
-# Samiksha Modi - 2019331
-# Vasu Yadav - 2019344
 opcode_table = {'CLA': '0000', 'LAC': '0001', 'SAC': '0010', 'ADD': '0011', 'SUB': '0100', 'BRZ': '0101',
                 'BRN': '0110', 'BRP': '0111', 'INP': '1000', 'DSP': '1001', 'MUL': '1010', 'DIV': '1011', 'STP': '1100'}
+length_of_inst = {'CLA': 1, 'LAC': 2, 'SAC': 2, 'ADD': 2, 'SUB': 2, 'BRZ': 2,
+                  'BRN': 2, 'BRP': 2, 'INP': 2, 'DSP': 2, 'MUL': 2, 'DIV': 2, 'STP': 1}
 symbol_table = {}
-label_table = {}
 declare_table = []
-END_flag = False  # False if no END in program, True if END is present in program
-i = 1
-program_counter = 0
-
-
-def declaration_table(input_file):
-    global declare_table
-    temp_table = []
-    # Appending the 'DC' 'DS line in declare_table
-    for line in input_file:
-        if len(line[0]) == 2:
-            declare_table.append(line)
-
-    # Removing 'DC' 'DS' line from input file by storing it in res then return res to input_file
-    res = [tt for tt in input_file if tt not in declare_table]
-
-    # Appending just the variable name from declare_table to temp_table
-    for line in declare_table:
-        temp_table.append(line[1])
-
-    # Copying temp_table to declare_table
-    declare_table = temp_table
-
-    # Removing '=' sign from elements in declare_table
-    for i, element in enumerate(declare_table):
-        if '=' in element:
-            declare_table[i] = element[:element.find('=')]
-
-    # Checking if any symbol has been declared twice and removing it
-    for element in declare_table:
-        temp = declare_table.count(element)
-        if temp > 1:
-            error_file.write(
-                "\n Symbol defined more than once: " + str(element))
-            cnt = 1
-            while cnt < temp:
-                declare_table.remove(element)
-                cnt = cnt+1
-
-    return res
+global input_file
+global location_counter
+ilc = 256
 
 
 def to_binary(data):
     return('{:012b}'.format(int(data)))
 
 
-def process(input_file):
+def process():
     # Read it one list element at a time
     for line in input_file:
         # Remove lines having only comment
@@ -83,159 +45,89 @@ def process(input_file):
     return temp2
 
 
-def pass_one(input_file):
-    # Builds the label table and the symbol table
-    global program_counter
-    global END_flag
-    global i
-    for j, line in enumerate(input_file):
+def declaration_table():
+    global declare_table
+    temp_table = []
+    # Appending the 'DC' 'DS line in declare_table
+    while len(input_file[-1]) == 3 and (input_file[-1][1] == 'DS' or input_file[-1][1] == 'DC'):
+        declare_table.append(input_file[-1][0])
+        input_file.remove(input_file[-1])
+
+    # Checking if any symbol has been declared twice and removing it
+    for element in declare_table:
+        temp = declare_table.count(element)
+        if temp > 1:
+            error_file.write(
+                "\n Symbol defined more than once: " + str(element))
+            cnt = 1
+            while cnt < temp:
+                declare_table.remove(element)
+                cnt = cnt+1
+
+
+def pass_one():
+    global location_counter
+    global ilc
+    for line in input_file:
         if line[0][-1] == ':':  # The line has a label
-            label_table[line[0][:-1]] = j+program_counter
-
-        if(line[0][-1] == ':'):  # The line has a label
-            if(line[1] == 'CLA' or line[1] == 'STP' or line[1] == 'END'):
-                if(line[1] == 'END'):
-                    END_flag = True
-                if len(line) > 2:
-                    error_file.write("\n Too many operands"+str(line))
-            elif (line[1] in opcode_table):
-                if len(line) > 3:
-                    error_file.write("\n Too many operands"+str(line))
-                if len(line) < 3:
-                    error_file.write("\n Too few operands"+str(line))
-                if(line[2] not in label_table):
-                    if(line[2] not in symbol_table):
-                        if(line[2].isdigit()):
-                            symbol_table[line[2]] = int(line[2])
-                        else:
-                            symbol_table[line[2]] = 256+i
-                            i += 1
+            if line[0][:-1] in symbol_table:
+                error_file.write(
+                    "\n Symbol defined more than once: " + str(line[0][:-1]))
             else:
-                error_file.write("\n Invalid opcode"+str(line))
-                input_file.remove(line)
+                # Add label to symbol table
+                symbol_table[line[0][:-1]] = location_counter
+                location_counter += 1
 
-        else:  # The line does not have a label
-            if(line[0] == 'CLA' or line[0] == 'STP' or line[0] == 'END'):
-                if(line[0] == 'END'):
-                    END_flag = True
-                if len(line) > 1:
-                    error_file.write("\n Too many operands"+str(line))
-            elif (line[0] in opcode_table):
-                if len(line) > 2:
-                    error_file.write("\n Too many operands"+str(line))
-                if len(line) < 2:
-                    error_file.write("\n Too few operands"+str(line))
-                if(line[1] not in label_table):
-                    if(line[1] not in symbol_table):
-                        if(line[1].isdigit()):
-                            symbol_table[line[1]] = int(line[1])
-                        else:
-                            symbol_table[line[1]] = 256+i
-                            i += 1
+            if len(line) == 2:
+                if (line[1] != 'CLA' and line[1] != 'STP' and line[1] not in opcode_table):
+                    error_file.write("\n Invalid opcode: " + str(line[1]))
+                    location_counter += 1
+                    continue
+                else:
+                    if(len(line) <= length_of_inst[line[1]]):
+                        error_file.write("\n Too few operands: " + str(line))
+                    location_counter += 1
+                    continue
+
+            if(line[1] in opcode_table):  # Checking if it is a valid opcode
+                if(len(line) > length_of_inst[line[1]]+1):
+                    error_file.write("\n Too many operands: " + str(line))
+                symbol_table[line[2]] = ilc  # Add variable to symbol table
+                ilc += 1
             else:
-                error_file.write("\n Invalid opcode"+str(line))
-                input_file.remove(line)
+                error_file.write("\n Invalid opcode: " + str(line[1]))
 
-    if(END_flag == False):
-        error_file.write("\n END statement is missing")
-
-    print("\n Label table: ", label_table)
-    print("\n Symbol table: ", symbol_table)
-
-    for element in symbol_table:
-        error_flag = True  # assuming error is there that is a symbol in the symbol is not defined. That is a symbol in the symbol table is not present in declare table
-        if element in declare_table:
-            error_flag = False
-        if error_flag == True:
-            error_file.write("\n Symbol used but not defined: " + str(element))
-
-    # Add label table to the symbol table
-    symbol_table.update(label_table)
-    print("\n Updated symbol table: ", symbol_table)
-
-
-"""
-def pass_two(input_file):
-    # Layout in memory
-    global program_counter
-    for program_counter, line in enumerate(input_file):
-        if(line[0][-1] == ':'):  # The line has a label
-            if(line[1] == 'CLA' or line[1] == 'STP'):
-                output_file.write("\n"+to_binary(program_counter))
-                output_file.write("\t"+opcode_table[line[1]])
+        elif len(line) > 1 and (line[1] == 'DS' or line[1] == 'DC'):
+            if line[0] in declare_table:
+                error_file.write(
+                    "\n Symbol defined more than once: " + str(line[0]))
             else:
-                output_file.write("\n"+to_binary(program_counter))
-                output_file.write("\t"+opcode_table[line[1]])
-                output_file.write("\t"+to_binary(str(symbol_table[line[2]])))
-        else:  # The line does not have a label
-            if(line[0] == 'CLA' or line[0] == 'STP'):
-                output_file.write("\n"+to_binary(program_counter))
-                output_file.write("\t"+opcode_table[line[0]])
+                declare_table.append(line[0])
+                location_counter += 1
+
+        else:  # There is no label
+            if len(line) == 1:
+                if (line[0] != 'CLA' and line[0] != 'STP' and line[0] not in opcode_table):
+                    error_file.write("\n Invalid opcode: " + str(line[0]))
+                    location_counter += 1
+                    continue
+                else:
+                    if(len(line) < length_of_inst[line[0]]):
+                        error_file.write("\n Too few operands: " + str(line))
+                    location_counter += 1
+                    continue
+            if(line[0] in opcode_table):  # Checking if it is a valid opcode
+                if(len(line) > length_of_inst[line[0]]):
+                    error_file.write("\n Too many operands: " + str(line))
+                location_counter += 1
+                if line[1] not in symbol_table:
+                    symbol_table[line[1]] = ilc  # Add variable to symbol table
+                    ilc += 1
             else:
-                output_file.write("\n"+to_binary(program_counter))
-                output_file.write("\t"+opcode_table[line[0]])
-                output_file.write("\t"+to_binary(str(symbol_table[line[1]])))"""
+                error_file.write("\n Invalid opcode: " + str(line[0]))
 
 
-def pass_two(input_file):
-    global program_counter
-    for program_counter, line in enumerate(input_file):
-        if(line[0][-1] == ':'):  # The line has a label
-            if(line[1] == 'END'):
-                continue
-            if(line[1] == 'CLA' or line[1] == 'STP'):
-                # output_file.write("\n"+to_binary(program_counter))
-                output_file.write("\n"+opcode_table[line[1]])
-            else:
-                # output_file.write("\n"+to_binary(program_counter))
-                output_file.write("\n"+opcode_table[line[1]])
-                output_file.write("\t"+to_binary(str(symbol_table[line[2]])))
-        else:  # The line does not have a label
-            if(line[0] == 'END'):
-                continue
-            if(line[0] == 'CLA' or line[0] == 'STP'):
-                # output_file.write("\n"+to_binary(program_counter))
-                output_file.write("\n"+opcode_table[line[0]])
-            else:
-                # output_file.write("\n"+to_binary(program_counter))
-                output_file.write("\n"+opcode_table[line[0]])
-                output_file.write("\t"+to_binary(str(symbol_table[line[1]])))
-
-
-def other_errors(input_file):
-    # Error: symbol not used
-    for element in label_table:
-        # error_flag is True if the element in label table is not found as an operand
-        error_flag = True
-        for line in input_file:
-            if(line[0][-1] == ':'):  # The line has a label
-                if(line[1] != 'CLA' and line[1] != 'STP' and line[1] != 'END'):
-                    # error_flag is made False if the element in label table is found as an operand
-                    if element == line[2]:
-                        error_flag = False
-                        break
-            else:  # The line does not have a label
-                if(line[0] != 'CLA' and line[0] != 'STP' and line[0] != 'END'):
-                    # error_flag is made False if the element in label table is found as an operand
-                    if element == line[1]:
-                        error_flag = False
-                        break
-
-        if error_flag == True:
-            error_file.write("\n Symbol not used: "+str(element))
-
-    # Error symbol defined more than once
-    for element in label_table:
-        error_flag = 0  # error_flag is 0 denoting the element has occurred 0 times so far
-        for line in input_file:
-            # The line has a label and element in label table is found in the program
-            if(line[0][-1] == ':' and line[0][:-1] == element):
-                error_flag = error_flag+1   # Increasing error_flag count by 1
-
-        if error_flag > 1:  # element in label table occurs more than once
-            error_file.write("\n Symbol defined more than once "+str(element))
-
-
+# TODO error used but not defined in not in declare table OR in symbol table.
 # Erasing output.txt file every time the program is run
 open("output.txt", "w").close()
 output_file = open("output.txt", "a")
@@ -245,8 +137,8 @@ open("error.txt", "w").close()
 error_file = open("error.txt", "a")
 
 # Takes the file name where the assembly language program is stored
-input_file_name = input("Enter input file name: ")
-# input_file_name = "input.txt"
+#input_file_name = input("Enter input file name: ")
+input_file_name = "input.txt"
 
 try:
     input_file = open(input_file_name, "r")
@@ -262,32 +154,31 @@ input_file = input_file.split("\n")
 print("\n", input_file)
 
 # Removes the comments and empty lines
-input_file = process(input_file)
-
-# Stores the symbols that have been declared and are not labels
-input_file = declaration_table(input_file)
-print("\n Declare table: ", declare_table)
+input_file = process()
 
 # Checks if START is missing. If missing, it reports the error. If present it removes it from input_file list
 if input_file[0][0] == 'START':
     if (len(input_file[0])) > 1:
-        program_counter = int(input_file[0][1])
-    input_file.remove(input_file[0])
+        location_counter = int(input_file[0][1])
+        input_file.remove(input_file[0])
 
 else:
+    location_counter = 0
     error_file.write("\n START statement is missing")
+
+# Checks if END is missing. If missing, it reports the error. If present it removes it from input_file list
+if input_file[-1][0] == 'END':
+    input_file.remove(input_file[-1])
+else:
+    error_file.write("\n END statement is missing")
+
+
+# Stores the symbols that have been declared and are not labels
+# declaration_table()
+#print("\n Declare table: ", declare_table)
 
 print("\n", input_file)
 
-# Checking if the no of instructions exceed the maximum no of instructions
-if(len(input_file)+program_counter > 256):
-    error_file.write("\n Memory address of instructions exceed 256")
-
-# Calls pass_one of the assembler
-pass_one(input_file)
-
-# Finds other errors in the assembly language program like symbol not used
-other_errors(input_file)
-
-# Write the machine code in output.txt
-pass_two(input_file)
+pass_one()
+print(symbol_table)
+print("\n Declare table: ", declare_table)
